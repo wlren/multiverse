a4572b59f81b1e4f73f5e397c535cf0acbb0293e
wlren <weilinwork99@gmail.com>
2021-07-08 03:47:37 +0800

    Remove temp local data

--- lib/repository/dining_repository.dart
+++ lib/repository/dining_repository.dart
@@ -17,43 +17,6 @@ class DiningRepository {
 
   CollectionReference diningReference =
       FirebaseFirestore.instance.collection('dining');
-  //Temp local data since backend is not set up
-  static final FullDayMenu sampleMenu = FullDayMenu(
-    breakfast: Menu([
-      Meal(
-        Cuisine.western,
-        [
-          MealItem('Scrambled Egg'),
-          MealItem('Honey Ham'),
-          MealItem('Herb Potato'),
-        ],
-      ),
-      Meal(
-        Cuisine.asian,
-        [
-          MealItem('Fried Ipoh Hor Fun'),
-          MealItem('Nonya Curry Vegetables'),
-          MealItem('Steamed Egg w/ Century Egg'),
-        ],
-      ),
-      Meal(
-        Cuisine.vegetarian,
-        [
-          MealItem('Fried Ipoh Hor Fun'),
-          MealItem('Nonya Curry Vegetables'),
-          MealItem('Deep Fried Goose Skin'),
-        ],
-      ),
-      Meal(
-        Cuisine.malay,
-        [
-          MealItem('Local Fried Mee Hoon'),
-          MealItem('Penang Curry Chicken'),
-          MealItem('Stir Fried Long Beans w/ Anchovies'),
-        ],
-      ),
-    ]),
-  );
 
   Future<FullDayMenu> getMenu(DateTime date, String location) async {
     final day = weekDayAsString(date);
